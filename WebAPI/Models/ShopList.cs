71b37e84a5a5bd027281e46ec8c7cdca6f8c571a
sztodo <57600228+sztodo@users.noreply.github.com>
2022-01-06 14:21:37 +0200

    lab 12.1

--- WebAPI/Models/ShopList.cs
+++ WebAPI/Models/ShopList.cs
@@ -7,5 +7,8 @@ namespace WebAPI.Models
 {
     public class ShopList
     {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
     }
 }
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +10,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Data;
 
 namespace WebAPI
 {
@@ -26,6 +28,9 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<WebAPIContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WebAPIContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
