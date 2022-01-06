061d4502db5c568dc29765d48eef16baf1b44d2a
sztodo <57600228+sztodo@users.noreply.github.com>
2022-01-06 14:32:13 +0200

    lab12.3

---
+++ Sztodolnik_Miha_Lab12/Sztodolnik_Miha_Lab12/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sztodolnik_Miha_Lab12.Models;
+using System.Threading.Tasks;
+
+namespace Sztodolnik_Miha_Lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
---
+++ Sztodolnik_Miha_Lab12/Sztodolnik_Miha_Lab12/Models/ShopList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sztodolnik_Miha_Lab12.Models
+{
+    class ShopList
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
