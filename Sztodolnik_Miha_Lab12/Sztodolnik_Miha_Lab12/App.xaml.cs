288d084e7a0e7316ec20bd0b5970c7efb517270d
sztodo <57600228+sztodo@users.noreply.github.com>
2022-01-06 14:25:35 +0200

    lab12.2

---
+++ Sztodolnik_Miha_Lab12/Sztodolnik_Miha_Lab12/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sztodolnik_Miha_Lab12
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
